8eea94341fdbf23d639ae5a3d0f828be3dfa2016
mouahrara <mouahrara@users.noreply.github.com>
2026-03-26 14:53:35 +0100

    Add compatibility with Carpentry Catalogue and Fast Farm Fabrications

--- Flip Buildings/srcs/Patches/Menus/CarpenterMenu.cs
+++ Flip Buildings/srcs/Patches/Menus/CarpenterMenu.cs
@@ -48,8 +48,8 @@ namespace FlipBuildings.Patches
 				postfix: new HarmonyMethod(typeof(CarpenterMenuPatch), nameof(DrawPostfix))
 			);
 			harmony.Patch(
-				original: AccessTools.Method(typeof(CarpenterMenu), nameof(CarpenterMenu.UpdateAppearanceButtonVisibility)),
-				postfix: new HarmonyMethod(typeof(CarpenterMenuPatch), nameof(UpdateAppearanceButtonVisibilityPostfix))
+				original: AccessTools.Method(typeof(CarpenterMenu), "resetBounds"),
+				postfix: new HarmonyMethod(typeof(CarpenterMenuPatch), nameof(ResetBoundsPostfix))
 			);
 			harmony.Patch(
 				original: AccessTools.Method(typeof(CarpenterMenu), nameof(CarpenterMenu.performHoverAction), new Type[] { typeof(int), typeof(int) }),
@@ -131,135 +131,17 @@ namespace FlipBuildings.Patches
 			}
 		}
 
-		private static void UpdateAppearanceButtonVisibilityPostfix(CarpenterMenu __instance)
+		private static void ResetBoundsPostfix(CarpenterMenu __instance)
 		{
-			__instance.backButton.myID = CarpenterMenu.region_backButton;
-			__instance.forwardButton.myID = CarpenterMenu.region_forwardButton;
-			__instance.appearanceButton.myID = CarpenterMenu.region_appearanceButton;
-			FlipButton = new ClickableTextureComponent("Flip", new Rectangle(__instance.xPositionOnScreen + __instance.width - IClickableMenu.borderWidth - IClickableMenu.spaceToClearSideBorder - 384 - 20, __instance.yPositionOnScreen + __instance.maxHeightOfBuildingViewer + 64, 64, 64), null, null, AssetManager.flipButton, new Rectangle(0, 0, 16, 16), 4f)
+			FlipButton = new ClickableTextureComponent("Flip", new Rectangle(__instance.xPositionOnScreen + __instance.width - IClickableMenu.borderWidth - IClickableMenu.spaceToClearSideBorder - 384 - 22, __instance.yPositionOnScreen + __instance.maxHeightOfBuildingViewer + 64, 64, 64), null, null, AssetManager.flipButton, new Rectangle(0, 0, 16, 16), 4f)
 			{
 				myID = region_flipButton,
-				rightNeighborID = CarpenterMenu.region_paintButton,
-				leftNeighborID = CarpenterMenu.region_appearanceButton,
+				leftNeighborID = ClickableComponent.SNAP_AUTOMATIC,
+				rightNeighborID = ClickableComponent.SNAP_AUTOMATIC,
 				upNeighborID = CarpenterMenu.region_appearanceButton,
 				visible = !__instance.readOnly && (Game1.IsMasterGame || Game1.player.team.farmhandsCanMoveBuildings.Value == FarmerTeam.RemoteBuildingPermissions.On || (Game1.player.team.farmhandsCanMoveBuildings.Value == FarmerTeam.RemoteBuildingPermissions.OwnedBuildings && __instance.TargetLocation.buildings.Any(building => BuildingUtility.CanBeFlipped(building))))
 			};
-			__instance.paintButton.myID = CarpenterMenu.region_paintButton;
-			__instance.moveButton.myID = CarpenterMenu.region_moveBuitton;
-			__instance.upgradeIcon.myID = CarpenterMenu.region_upgradeIcon;
-			__instance.okButton.myID = CarpenterMenu.region_okButton;
-			__instance.demolishButton.myID = CarpenterMenu.region_demolishButton;
-			__instance.cancelButton.myID = CarpenterMenu.region_cancelButton;
-			__instance.backButton.leftNeighborID = -1;
-			__instance.backButton.rightNeighborID = __instance.forwardButton.myID;
-			__instance.forwardButton.leftNeighborID = __instance.backButton.myID;
-			__instance.forwardButton.rightNeighborID = __instance.appearanceButton.myID;
-			__instance.appearanceButton.leftNeighborID = __instance.forwardButton.myID;
-			__instance.appearanceButton.rightNeighborID = FlipButton.myID;
-			FlipButton.leftNeighborID = __instance.appearanceButton.myID;
-			FlipButton.rightNeighborID = __instance.paintButton.myID;
-			__instance.paintButton.leftNeighborID = FlipButton.myID;
-			__instance.paintButton.rightNeighborID = __instance.moveButton.myID;
-			__instance.moveButton.leftNeighborID = __instance.paintButton.myID;
-			__instance.moveButton.rightNeighborID = __instance.okButton.myID;
-			__instance.upgradeIcon.leftNeighborID = __instance.moveButton.myID;
-			__instance.upgradeIcon.rightNeighborID = __instance.okButton.myID;
-			__instance.okButton.leftNeighborID = __instance.moveButton.myID;
-			__instance.okButton.rightNeighborID = __instance.demolishButton.myID;
-			__instance.demolishButton.leftNeighborID = __instance.okButton.myID;
-			__instance.demolishButton.rightNeighborID = __instance.cancelButton.myID;
-			__instance.cancelButton.leftNeighborID = __instance.demolishButton.myID;
-			__instance.cancelButton.rightNeighborID = -1;
-
-			if (!__instance.paintButton.visible && !__instance.moveButton.visible && !__instance.okButton.visible && !__instance.demolishButton.visible)
-			{
-				FlipButton.visible = false;
-			}
-			if (!__instance.appearanceButton.visible && !FlipButton.visible && !__instance.paintButton.visible && !__instance.moveButton.visible && !__instance.okButton.visible)
-			{
-				__instance.forwardButton.rightNeighborID = __instance.cancelButton.myID;
-				__instance.cancelButton.leftNeighborID = __instance.forwardButton.myID;
-			}
-			else if (!FlipButton.visible && !__instance.paintButton.visible && !__instance.moveButton.visible && !__instance.okButton.visible)
-			{
-				__instance.appearanceButton.rightNeighborID = __instance.cancelButton.myID;
-				__instance.cancelButton.leftNeighborID = __instance.appearanceButton.myID;
-			}
-			else
-			{
-				if (!__instance.appearanceButton.visible && !FlipButton.visible && !__instance.paintButton.visible && !__instance.moveButton.visible)
-				{
-					__instance.forwardButton.rightNeighborID = __instance.okButton.myID;
-					__instance.okButton.leftNeighborID = __instance.forwardButton.myID;
-				}
-				else if (!FlipButton.visible && !__instance.paintButton.visible && !__instance.moveButton.visible)
-				{
-					__instance.appearanceButton.rightNeighborID = __instance.okButton.myID;
-					__instance.okButton.leftNeighborID = __instance.appearanceButton.myID;
-				}
-				else if (!__instance.appearanceButton.visible && !__instance.paintButton.visible && !__instance.moveButton.visible)
-				{
-					__instance.forwardButton.rightNeighborID = FlipButton.myID;
-					FlipButton.leftNeighborID = __instance.forwardButton.myID;
-					FlipButton.rightNeighborID = __instance.okButton.myID;
-					__instance.okButton.leftNeighborID = FlipButton.myID;
-				}
-				else if (!__instance.appearanceButton.visible && !FlipButton.visible && !__instance.moveButton.visible)
-				{
-					__instance.forwardButton.rightNeighborID = __instance.paintButton.myID;
-					__instance.paintButton.leftNeighborID = __instance.forwardButton.myID;
-					__instance.paintButton.rightNeighborID = __instance.okButton.myID;
-					__instance.okButton.leftNeighborID = __instance.paintButton.myID;
-				}
-				else if (!__instance.appearanceButton.visible && !FlipButton.visible && !__instance.paintButton.visible)
-				{
-					__instance.forwardButton.rightNeighborID = __instance.moveButton.myID;
-					__instance.moveButton.leftNeighborID = __instance.forwardButton.myID;
-				}
-				else if (!__instance.paintButton.visible && !__instance.moveButton.visible)
-				{
-					FlipButton.rightNeighborID = __instance.okButton.myID;
-					__instance.okButton.leftNeighborID = FlipButton.myID;
-				}
-				else if (!FlipButton.visible && !__instance.paintButton.visible)
-				{
-					__instance.appearanceButton.rightNeighborID = __instance.moveButton.myID;
-					__instance.moveButton.leftNeighborID = __instance.appearanceButton.myID;
-				}
-				else if (!__instance.appearanceButton.visible && !FlipButton.visible)
-				{
-					__instance.forwardButton.rightNeighborID = __instance.paintButton.myID;
-					__instance.paintButton.leftNeighborID = __instance.forwardButton.myID;
-				}
-				else
-				{
-					if (!__instance.moveButton.visible)
-					{
-						__instance.paintButton.rightNeighborID = __instance.okButton.myID;
-						__instance.okButton.leftNeighborID = __instance.paintButton.myID;
-					}
-					else if (!__instance.paintButton.visible)
-					{
-						FlipButton.rightNeighborID = __instance.moveButton.myID;
-						__instance.moveButton.leftNeighborID = FlipButton.myID;
-					}
-					if (!FlipButton.visible)
-					{
-						__instance.appearanceButton.rightNeighborID = __instance.paintButton.myID;
-						__instance.paintButton.leftNeighborID = __instance.appearanceButton.myID;
-					}
-					else if (!__instance.appearanceButton.visible)
-					{
-						__instance.forwardButton.rightNeighborID = FlipButton.myID;
-						FlipButton.leftNeighborID = __instance.forwardButton.myID;
-					}
-				}
-				if (!__instance.demolishButton.visible)
-				{
-					__instance.okButton.rightNeighborID = __instance.demolishButton.rightNeighborID;
-					__instance.cancelButton.leftNeighborID = __instance.demolishButton.leftNeighborID;
-				}
-			}
+			__instance.cancelButton.leftNeighborID = ClickableComponent.SNAP_AUTOMATIC;
 		}
 
 		private static bool PerformHoverActionPrefix(CarpenterMenu __instance, int x, int y)
